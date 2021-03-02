24267ea69ae70cd57ef261a0a9387d03b9cc6b33
Soichiro Doi <dschr1031463@gmail.com>
2021-03-02 14:57:54 +0900

    feat: backprop method

--- DeZero/core.py
+++ DeZero/core.py
@@ -1,6 +1,7 @@
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 
 class Function:
@@ -8,13 +9,15 @@ class Function:
         x = input.data
         y = self.forward(x)    # forward method implements specific computation
         output = Variable(y)
+        self.input = input
         return output
 
     def forward(self, x):
         raise NotImplementedError()
 
+    def backward(self, gy):
+        raise NotImplementedError()
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
+
+
 
