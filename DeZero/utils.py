083c9b5709f637ca04d131c700faffc61773b3ea
Soichiro Doi <dschr1031463@gmail.com>
2021-03-11 18:14:54 +0900

    visualize graphs

---
+++ DeZero/utils.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+
+
+# VariableインスタンスをDOT言語に変換する
+# verbose = True でインスタンスの形状と型もラベルとともに表示できる
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+    return dot_var.format(id(v), name)
+
+
+# DeZero関数をDOT言語に変換する
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y()))
+    return txt
+
+
+# 全体をつなげる
+def get_dot_graph(output, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def add_func(f):
+        if f not in seen_set:
+            funcs.append(f)
+            # funcs.sort(key=lambda x: x.generation)
+            seen_set.add(f)
+
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+    return 'digraph g {\n' + txt + '}'
+
+
+# dotコマンドの実行までできる関数
+def plot_dot_graph(output, verbose=True, to_file='graph.png'):
+    got_graph = get_dot_graph(output, verbose)
+
+    # dot data download
+    tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+
+    with open(graph_path, 'w') as f:
+        f.write(got_graph)
+
+    # call dot command
+    extension = os.path.splitext(to_file)[1][1:]
+    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
+    subprocess.run(cmd, shell=True)
