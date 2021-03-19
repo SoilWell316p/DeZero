7e2f1820551268f50f2bfc16408efd88452e4dd3
Soichiro Doi <dschr1031463@gmail.com>
2021-03-20 03:17:56 +0900

    feat: layers for parameters

---
+++ Tests/test_layers.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+from DeZero.core import Variable, Parameter
+from DeZero.layers import Layer
+
+
+class TestPlaneLayers(unittest.TestCase):
+    def test_simple_layer(self):
+        layer = Layer()
+
+        layer.p1 = Parameter(np.array(1.0))
+        layer.p2 = Parameter(np.array(2.0))
+        layer.p3 = Variable(np.array(3.0))
+        layer.p4 = 'test'
+
+        self.assertEqual(layer._params, {'p2', 'p1'})
+
+
